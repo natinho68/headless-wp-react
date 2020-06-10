c72003902193283920f28df00d2dedcda7f0e675
Nathan MEYER <nmeyer@welcoming.com>
2020-06-10 18:45:51 +0200

    fix: error toast are stackable

--- src/components/ErrorToast.tsx
+++ src/components/ErrorToast.tsx
@@ -9,14 +9,16 @@ interface ErrorToastProps {
 const ErrorToast: React.FC<ErrorToastProps> = ({ errorTitle, errorMessage }) => {
   const [showError, setShowError] = useState(true)
   return (
-    <>
-      <Toast className={'toast-position'} show={showError} onClose={() => setShowError(!showError)}>
-        <Toast.Header className={'bg-danger text-white'}>
-          <strong className="mr-auto">{errorTitle}</strong>
-        </Toast.Header>
-        <Toast.Body className={'text-danger'}>{errorMessage}</Toast.Body>
-      </Toast>
-    </>
+    <div className={'toast-container'}>
+      <div className={'toast-position'}>
+        <Toast show={showError} onClose={() => setShowError(!showError)} delay={4000} autohide>
+          <Toast.Header className={'bg-danger text-white'}>
+            <strong className="mr-auto">{errorTitle}</strong>
+          </Toast.Header>
+          <Toast.Body className={'text-danger'}>{errorMessage}</Toast.Body>
+        </Toast>
+      </div>
+    </div>
   )
 }
 export default ErrorToast
